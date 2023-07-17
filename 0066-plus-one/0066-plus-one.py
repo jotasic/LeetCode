c640df9a84edc881abc69ec9b4a387b31f0008db
Taewoo Kim <embedded61@gmail.com>
2023-07-18 00:09:59 +0900

    Time: 55 ms (17.36%), Space: 16.3 MB (70.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -2,8 +2,7 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         
         l = len(digits) - 1
-        is_add_one = False
-        
+
         for i in range(l, -1, -1):
             number = digits[i] + 1
             
